c44ff41a6c4588d643a8e88e38ee6a5f52af1386
Ludwing Rivera Amador <ludwingnidoo@192.168.0.14>
2020-05-11 18:54:44 -0500

    Add project

---
+++ src/lib/bodyParser.js
@@ -0,0 +1,20 @@
+function bodyParser(request) {
+    return new Promise(async (resolve, reject) => {
+        let totalData = '';
+        request
+            .on('data', chunk => {
+                totalData += chunk;
+            })
+            .on('end', () => {
+                request.body = JSON.parse(totalData);
+                resolve();
+            })
+            .on('error', err => {
+                console.log(err);
+                reject();
+            })
+    });
+}
+
+
+module.exports = { bodyParser };
